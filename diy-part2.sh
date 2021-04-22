35a3d95f53f4f5eeaddf428e6167627a42ad0cf7
sxml2015 <70975028+sxml2015@users.noreply.github.com>
2021-04-22 21:59:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,9 +31,9 @@ git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # 编译 po2lmo (如果有po2lmo可跳过)
-pushd package/luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
+#pushd package/luci-app-openclash/tools/po2lmo
+#make && sudo make install
+#popd
 
 #Bypass
 #git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
