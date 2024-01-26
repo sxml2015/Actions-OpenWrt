0865467dc3174f8805135370ea61b9e853121c87
sxml2015 <70975028+sxml2015@users.noreply.github.com>
2024-01-26 15:32:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,79 +1,91 @@
 #!/bin/bash
 #============================================================
-# # 2021-11-08
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com 
+# 2024-01-19
+#https://github.com/HoldOnBro/Actions-OpenWrt
+#https://github.com/breakings/OpenWrt
 #============================================================
 #移除不用软件包
-#rm -rf package/lean/luci-app-dockerman
 rm -rf feeds/luci/applications/luci-app-dockerman
 #rm -rf package/lean/luci-app-wrtbwmon
 rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
+rm -fr feeds/luci/themes/luci-theme-design
+#20231010
+#rm -rf feeds/packages/utils/prometheus-node-exporter-lua
 
-#添加smartdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/luci-app-smartdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns package/smartdns
-#svn co https://github.com/liuran001/openwrt-packages/trunk/luci-app-smartdns package/luci-app-smartdns
-#svn co https://github.com/liuran001/openwrt-packages/trunk/smartdns package/smartdns
-#svn co https://github.com/sxml/openwrt-packages/trunk/smartdns package/smartdns
+# 克隆 kenzok8仓库
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages.git kenzok8-packages
+cp -rf kenzok8-packages/smartdns package/smartdns
+cp -rf kenzok8-packages/luci-app-smartdns package/luci-app-smartdns
+#cp -rf kenzok8-packages/gost package/gost
+#git clone --depth=1 https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone --depth=1 https://github.com/kenzok8/small-package.git small-package
+cp -rf small-package/gost package/gost
+cp -rf small-package/luci-app-gost package/luci-app-gost
+cp -rf small-package/sagernet-core package/sagernet-core
 
-#SSR-plus
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+# 克隆 fw876 仓库
+git clone --depth=1 -b main https://github.com/fw876/helloworld.git
+cp -rf helloworld/luci-app-ssr-plus package/luci-app-ssr-plus
+cp -rf helloworld/xray-core package/xray-core
+cp -rf helloworld/xray-plugin package/xray-plugin
+cp -rf helloworld/shadowsocks-rust package/shadowsocks-rust
+cp -rf helloworld/shadowsocksr-libev package/shadowsocksr-libev
+cp -rf helloworld/v2ray-plugin package/v2ray-plugin
+cp -rf helloworld/v2ray-core package/v2ray-core
+cp -rf helloworld/v2ray-geodata package/v2ray-geodata
+cp -rf helloworld/trojan package/trojan
+#cp -rf helloworld/sagernet-core package/sagernet-core
+cp -rf helloworld/microsocks package/microsocks
+cp -rf helloworld/redsocks2 package/redsocks2
+cp -rf helloworld/ipt2socks package/ipt2socks
+cp -rf helloworld/dns2socks package/dns2socks
+cp -rf helloworld/lua-neturl package/lua-neturl
+cp -rf helloworld/gn package/gn
+cp -rf helloworld/shadow-tls package/shadow-tls
+cp -rf helloworld/tuic-client package/tuic-client
+cp -rf helloworld/naiveproxy package/naiveproxy
+#rm -rf helloworld
+
+# 克隆openwrt-passwall仓库
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git
+cp -rf openwrt-passwall-packages/brook package/brook
+cp -rf openwrt-passwall-packages/ssocks package/ssocks
+cp -rf openwrt-passwall-packages/simple-obfs package/simple-obfs
+cp -rf openwrt-passwall-packages/pdnsd-alt package/pdnsd-alt
+cp -rf openwrt-passwall-packages/chinadns-ng package/chinadns-ng
+cp -rf openwrt-passwall-packages/tcping package/tcping
+cp -rf openwrt-passwall-packages/trojan-go package/trojan-go
+cp -rf openwrt-passwall-packages/trojan-plus package/trojan-plus
+cp -rf openwrt-passwall-packages/hysteria package/hysteria
+cp -rf openwrt-passwall-packages/dns2tcp package/dns2tcp
+cp -rf openwrt-passwall-packages/sing-box package/sing-box
+#rm -rf openwrt-passwall-packages
 
 #passwall
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git
+cp -rf openwrt-passwall/luci-app-passwall package/luci-app-passwall
+#rm -rf openwrt-passwall
 
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/ssocks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/brook
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/chinadns-ng
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/tcping
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/trojan-go
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/trojan-plus
-#svn co https://github.com/fw876/helloworld/trunk/xray-core package/xray-core
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-core package/xray-core
-#svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/xray-plugin
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-plugin package/xray-plugin
-#svn co https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/shadowsocks-rust
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/shadowsocks-rust package/shadowsocks-rust
-svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/shadowsocksr-libev
-#svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/v2ray-plugin
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-plugin package/v2ray-plugin
-#svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/v2ray-core
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-core package/v2ray-core
-#svn co https://github.com/fw876/helloworld/trunk/v2ray-geodata package/v2ray-geodata
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-geodata package/v2ray-geodata
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/pdnsd-alt package/pdnsd-alt
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria package/hysteria
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/simple-obfs package/simple-obfs
-#git clone https://github.com/sxml/luci-app-kcptun.git package/kcptun
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan package/trojan
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2tcp package/dns2tcp
-#svn co https://github.com/fw876/helloworld/trunk/sagernet-core package/sagernet-core
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/sagernet-core package/sagernet-core
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/microsocks package/microsocks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2socks package/dns2socks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ipt2socks package/ipt2socks
-svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/lua-neturl
+#passwall2
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git
+cp -rf openwrt-passwall2/luci-app-passwall2 package/luci-app-passwall2
+#rm -rf openwrt-passwall2
 
-#naiveproxy代理
-#svn co https://github.com/fw876/helloworld/trunk/naiveproxy package/naiveproxy
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy package/naiveproxy
 #解析
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 
 #文件助手
-git clone https://github.com/sxml/luci-app-fileassistant.git package/luci-app-fileassistant
-#报错svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-fileassistant package/luci-app-fileassistant
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-fileassistant package/luci-app-fileassistant
+git clone --depth=1 https://github.com/Lienol/openwrt-package.git fileassistant
+cp -rf fileassistant/luci-app-fileassistant package/luci-app-fileassistant
+#git clone https://github.com/sxml/luci-app-fileassistant.git package/luci-app-fileassistant
+#rm -rf fileassistant
 
 #添加luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git
+cp -rf luci-app-amlogic/luci-app-amlogic package/luci-app-amlogic
+#rm -rf luci-app-amlogic
+
 #修改晶晨宝盒默认配置
 # 1.设置OpenWrt 文件的下载仓库
 sed -i "s|https.*/OpenWrt|https://github.com/sxml/Actions-OpenWrt|g" package/luci-app-amlogic/root/etc/config/amlogic
@@ -86,17 +98,10 @@ sed -i "s|.img.gz|.img.gz|g" package/luci-app-amlogic/root/etc/config/amlogic
 sed -i "s|http.*/library|https://github.com/breakings/OpenWrt/tree/main/opt/kernel|g" package/luci-app-amlogic/root/etc/config/amlogic
 
 # themes 主题
-svn co https://github.com/Leo-Jo-My/luci-theme-opentomcat/trunk package/luci-theme-opentomcat
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-argonne package/luci-theme-argonne
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-tomato package/luci-theme-tomato
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-neobird package/luci-theme-neobird
-#svn co https://github.com/thinktip/luci-theme-neobird/trunk package/luci-theme-neobird
-
-#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-# 编译 po2lmo (如果有po2lmo可跳过)
-#pushd package/luci-app-openclash/tools/po2lmo
-#make && sudo make install
-#popd
+git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone --depth=1 https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+git clone --depth=1 https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
+git clone --depth=1 https://github.com/gngpp/luci-app-design-config.git package/luci-app-design-config
 
 #修改makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/luci\.mk/include \$(TOPDIR)\/feeds\/luci\/luci\.mk/g' {}
@@ -104,38 +109,8 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHREPO/PKG_SOURCE_URL:=https:\/\/github\.com/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload\.github\.com/g' {}
 
+# 临时修复xfsprogs
+#sed -i 's/TARGET_CFLAGS += -DHAVE_MAP_SYNC/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/' feeds/packages/utils/xfsprogs/Makefile
+
 ./scripts/feeds update -a
-./scripts/feeds install -a
-
-# 参考
-#阿里云盘
-#svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-webdav package/aliyundrive-webdav
-#svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav package/luci-app-aliyundrive-webdav
-# luci-app-mosdns
-#svn co https://github.com/QiuSimons/openwrt-mos/trunk/luci-app-mosdns package/luci-app-mosdns
-#svn co https://github.com/QiuSimons/openwrt-mos/trunk/mosdns package/mosdns
-#京东
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-#kcptun
-#git clone https://github.com/sxml/luci-app-kcptun.git package/luci-app-kcptun
-
-#依IP地址限速
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-eqos package/luci-app-eqos
-
-# 流量监控
-#svn co https://github.com/brvphoenix/wrtbwmon/trunk/wrtbwmon package/wrtbwmon
-#git clone https://github.com/brvphoenix/luci-app-wrtbwmon
-#cd luci-app-wrtbwmon
-#git reset --hard ff7773abbf71120fc39a276393b29ba47353a7e2 && cp -r luci-app-wrtbwmon ../package/
-#cd ..
-
-# 编译缺少upx依赖 添加这看看
-#git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
-
-# python3  （修改Makefile）
-#sed -i 's/PYTHON3_SETUPTOOLS_VERSION:=.*/PYTHON3_SETUPTOOLS_VERSION:=63.1.0/g' feeds/packages/lang/python/python3-version.mk
-#sed -i 's/PYTHON3_PIP_VERSION:=.*/PYTHON3_PIP_VERSION:=22.1.2/g' feeds/packages/lang/python/python3-version.mk
-
-# python-cryptography （替换其他软件包 文件）
-#rm -rf feeds/packages/lang/python/python-cryptography
-#svn co https://github.com/openwrt/packages/trunk/lang/python/python-cryptography feeds/packages/lang/python/python-cryptography
+./scripts/feeds install -a -f
