8b7f9c5f2ae2026fc759f80740f92e30bab2a3db
sxml2015 <70975028+sxml2015@users.noreply.github.com>
2024-02-04 12:47:24 +0800

    Update getImgs.sh

--- aarch64/getImgs.sh
+++ aarch64/getImgs.sh
@@ -5,5 +5,5 @@
 #sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/Armbian_20.10_Aml-s9xxx_buster_5.4.73-flippy-47+o.img.xz
 #sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/Armbian_20.10_Beikeyun_buster_5.4.73-flippy-47+o.img.xz
 #sudo wget https://github.com/HoldOnBro/Actions-OpenWrt/releases/download/Files4Compile/Armbian_20.10_L1-Pro_buster_5.4.73-flippy-47+o.img.xz
-# Files 标签 ID 获取 （F大打包文件） https://api.github.com/repos/sxml/Actions-OpenWrt/releases/latest
+# Files 标签 ID 获取 （F大打包文件） https://api.github.com/repos/sxml2015/Actions-OpenWrt/releases/latest
 wget $(curl -s https://api.github.com/repos/sxml2015/Actions-OpenWrt/releases/138624163 | grep browser_download_url | cut -d '"' -f 4)
