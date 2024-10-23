7711e29adbcf6b547038582b62962e83675f9af4
sxml2015 <70975028+sxml2015@users.noreply.github.com>
2024-10-23 20:34:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #============================================================
-# sxml2015
-# 2024-02-16
+# sxml
+# 2024-10-22
 #https://github.com/HoldOnBro/Actions-OpenWrt
 #https://github.com/breakings/OpenWrt
 #============================================================
@@ -11,20 +11,19 @@ rm -rf feeds/luci/applications/luci-app-dockerman
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/applications/luci-app-smartdns
 rm -fr feeds/luci/themes/luci-theme-design
-#20231010
-#rm -rf feeds/packages/utils/prometheus-node-exporter-lua
 
 # 克隆 kenzok8仓库
 git clone --depth=1 https://github.com/kenzok8/openwrt-packages.git kenzok8-packages
 cp -rf kenzok8-packages/smartdns package/smartdns
 cp -rf kenzok8-packages/luci-app-smartdns package/luci-app-smartdns
+#cp -rf kenzok8-packages/luci-theme-argon package/luci-theme-argon
 #cp -rf kenzok8-packages/gost package/gost
 #git clone --depth=1 https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-git clone --depth=1 https://github.com/kenzok8/small-package.git small-package
-cp -rf small-package/gost package/gost
-cp -rf small-package/luci-app-gost package/luci-app-gost
+#git clone --depth=1 https://github.com/kenzok8/small-package.git small-package
+#cp -rf small-package/gost package/gost
+#cp -rf small-package/luci-app-gost package/luci-app-gost
 #cp -rf small-package/sagernet-core package/sagernet-core
-cp -rf small-package/v2ray-geodata package/v2ray-geodata
+#cp -rf small-package/v2ray-geodata package/v2ray-geodata
 
 # 克隆 fw876 仓库
 git clone --depth=1 -b main https://github.com/fw876/helloworld.git
@@ -62,6 +61,7 @@ cp -rf openwrt-passwall-packages/trojan-plus package/trojan-plus
 cp -rf openwrt-passwall-packages/hysteria package/hysteria
 cp -rf openwrt-passwall-packages/dns2tcp package/dns2tcp
 cp -rf openwrt-passwall-packages/sing-box package/sing-box
+cp -rf openwrt-passwall-packages/v2ray-geodata package/v2ray-geodata
 #rm -rf openwrt-passwall-packages
 
 #passwall
@@ -93,39 +93,33 @@ sed -i "s|.img.gz|.img.gz|g" package/luci-app-amlogic/root/etc/config/amlogic
 sed -i "s|http.*/library|https://github.com/breakings/OpenWrt/tree/main/opt/kernel|g" package/luci-app-amlogic/root/etc/config/amlogic
 
 # themes 主题
-git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-git clone --depth=1 https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
-git clone --depth=1 https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
-git clone --depth=1 https://github.com/gngpp/luci-app-design-config.git package/luci-app-design-config
+#git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+#git clone --depth=1 https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+#git clone --depth=1 https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
+#git clone --depth=1 https://github.com/gngpp/luci-app-design-config.git package/luci-app-design-config
+#git clone --depth=1 https://github.com/sxml/luci-theme-design.git package/luci-theme-design
+#git clone --depth=1 https://github.com/sxml/luci-app-design-config.git package/luci-app-design-config
 
-# 临时修复xfsprogs
-sed -i 's|TARGET_CFLAGS += -DHAVE_MAP_SYNC.*|TARGET_CFLAGS += -DHAVE_MAP_SYNC $(if $(CONFIG_USE_MUSL),-D_LARGEFILE64_SOURCE)|' feeds/packages/utils/xfsprogs/Makefile
+#mosdns
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+#命令中的 -b v5-lua 的意思是指定要克隆的分支（branch）为 v5-lua
+git clone -b v5-lua https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+git clone -b v5-lua https://github.com/sbwml/luci-app-mosdns package/mosdns
 
-# cryptsetup
-#sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2.6.1/g' feeds/packages/utils/cryptsetup/Makefile
-#sed -i 's/PKG_HASH:=.*/PKG_HASH:=410ded65a1072ab9c8e41added37b9729c087fef4d2db02bb4ef529ad6da4693/g' feeds/packages/utils/cryptsetup/Makefile
-#sed -i '78i\TARGET_CFLAGS += -D_LARGEFILE64_SOURCE\' feeds/packages/utils/cryptsetup/Makefile
+#添加ddns-go 动态域名解析
+#git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+#git clone --depth=1 https://github.com/sxml/luci-app-ddns-go.git package/ddns-go
+git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go.git
+cp -rf luci-app-ddns-go/ddns-go package/ddns-go
+cp -rf luci-app-ddns-go/luci-app-ddns-go package/luci-app-ddns-go
 
-# perl
-rm -rf feeds/packages/lang/perl
-cp -rf $GITHUB_WORKSPACE/general/perl feeds/packages/lang
+#文件浏览器
+git clone --depth=1 https://github.com/sxml/luci-app-filebrowser.git package/luci-app-filebrowser
 
-# shadowsocks-rust
-#cp -rf $GITHUB_WORKSPACE/general/shadowsocks-rust package/shadowsocks-rust
-
-# curl
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=8.5.0/g' feeds/packages/net/curl/Makefile
-sed -i 's/PKG_HASH:=.*/PKG_HASH:=ce4b6a6655431147624aaf582632a36fe1ade262d5fab385c60f78942dd8d87b/g' feeds/packages/net/curl/Makefile
-sed -i 's/PKG_RELEASE:=.*/PKG_RELEASE:=1/g' feeds/packages/net/curl/Makefile
-#rm -rf feeds/packages/net/curl
-
-# 修改libxml2的Makefile，启用shared
-sed -i '/HOST_CONFIGURE_ARGS/ s/--disable-shared/--enable-shared/' feeds/packages/libs/libxml2/Makefile
-# 修改libxslt的Makefile，指定libxml的库路径和头文件路径
-sed -i '/HOST_CONFIGURE_ARGS/ a\--with-libxml-libs-prefix=$(STAGING_DIR_HOSTPKG)/lib' feeds/packages/libs/libxslt/Makefile
-sed -i '/HOST_CONFIGURE_ARGS/ a\--with-libxml-include-prefix=$(STAGING_DIR_HOSTPKG)/include/libxml2/' feeds/packages/libs/libxslt/Makefile
-# 修改xsltproc的Makefile，链接libxml2库
-sed -i 's|LIBXML_LIBS =|LIBXML_LIBS = -lxml2 -L$(STAGING_DIR_HOSTPKG)/lib|' build_dir/hostpkg/libxslt-1.1.34/xsltproc/Makefile
+#lrzsz
+rm -rf feeds/packages/utils/lrzsz
+cp -rf $GITHUB_WORKSPACE/general/lrzsz feeds/packages/utils
 
 #修改makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/luci\.mk/include \$(TOPDIR)\/feeds\/luci\/luci\.mk/g' {}
