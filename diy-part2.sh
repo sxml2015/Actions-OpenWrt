e9d07490c0a935fa786fd282527ce69e00317a46
sxml2015 <70975028+sxml2015@users.noreply.github.com>
2024-10-25 07:22:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -116,6 +116,9 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddn
 #文件浏览器
 #git clone --depth=1 https://github.com/sxml/luci-app-filebrowser.git package/luci-app-filebrowser
 
+#文件助手
+git clone --depth=1 https://github.com/sxml/luci-app-fileassistant.git package/luci-app-fileassistant
+
 #lrzsz
 #rm -rf feeds/packages/utils/lrzsz
 #cp -rf $GITHUB_WORKSPACE/general/lrzsz feeds/packages/utils
